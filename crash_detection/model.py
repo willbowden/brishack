ca6376890b6efd591f9d0f57eaab69cca0d2b33c
Will <willbowden03@yahoo.co.uk>
2023-02-27 01:54:50 +0000

    crash detection uwu

--- crash_detection/model.py
+++ crash_detection/model.py
@@ -1,21 +1,63 @@
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense, LSTM, Dropout
-from tensorflow.keras.optimizers import SGD
-import json
+import cv2
+import numpy as np # linear algebra
+import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+import matplotlib.pyplot as plt
+import tensorflow as tf
+from tensorflow.keras import layers
 
+batch_size = 100
+img_height = 250
+img_width = 250
 
-# def create_model(modelName: str):
-#     with open(f"{modelName}.json", "r") as infile:
-#       config = json.load(infile)
+training_ds = tf.keras.preprocessing.image_dataset_from_directory(
+    './data/train',
+    seed=101,
+    image_size= (img_height, img_width),
+    batch_size=batch_size
 
-#     model = Sequential()
-#     model.add(LSTM(config['units'], return_sequences=True, batch_input_shape=(None, config['sequence_length'], config['n_features'])))
-#     model.add(Dropout(config['dropout']))
-#     model.add(LSTM(config['units'], return_sequences=False))
-#     model.add(Dropout(config['dropout']))
-#     model.add(Dense(1, activation="linear"))
+)
 
-#     model.compile(loss=config['loss'], metrics=["accuracy", "mean_absolute_error"], optimizer=config['optimizer'])
-#     model_path = f"{modelName}"
-#     model.save(model_path)
-#     return 
+testing_ds = tf.keras.preprocessing.image_dataset_from_directory(
+    './data/test',
+    seed=101,
+    image_size= (img_height, img_width),
+    batch_size=batch_size)
+
+validation_ds =  tf.keras.preprocessing.image_dataset_from_directory(
+    './data/val',
+    seed=101,
+    image_size= (img_height, img_width),
+    batch_size=batch_size)
+
+class_names = training_ds.class_names
+
+## Configuring dataset for performance
+AUTOTUNE = tf.data.experimental.AUTOTUNE
+training_ds = training_ds.cache().prefetch(buffer_size=AUTOTUNE)
+testing_ds = testing_ds.cache().prefetch(buffer_size=AUTOTUNE)
+
+img_shape = (img_height, img_width, 3)
+
+base_model = tf.keras.applications.MobileNetV2(input_shape=img_shape,
+                                               include_top=False,
+                                               weights='imagenet')
+
+base_model.trainable = False
+
+model = tf.keras.Sequential([
+    base_model,
+    layers.Conv2D(32, 3, activation='relu'),
+    layers.Conv2D(64, 3, activation='relu'),
+    layers.Conv2D(128, 3, activation='relu'),
+    layers.Flatten(),
+    layers.Dense(len(class_names), activation= 'softmax')
+])
+
+model.compile(optimizer='adam',loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+
+history = model.fit(training_ds, validation_data = validation_ds, epochs = 50)
+
+plt.plot(history.history['loss'], label = 'training loss')
+plt.plot(history.history['accuracy'], label = 'training accuracy')
+plt.grid(True)
+plt.legend()
